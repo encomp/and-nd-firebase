0cc34bd2f5c82c8258735da6362666b5272e60f5
encomp <edgar.martinez.rico@gmail.com>
2018-12-31 12:15:03 -0800

    Fixing java style.

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -1,16 +1,14 @@
 /**
  * Copyright Google Inc. All Rights Reserved.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.google.firebase.udacity.friendlychat;
@@ -38,75 +36,76 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+  private static final String TAG = "MainActivity";
 
-    public static final String ANONYMOUS = "anonymous";
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+  public static final String ANONYMOUS = "anonymous";
+  public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
 
-    private ListView mMessageListView;
-    private MessageAdapter mMessageAdapter;
-    private ProgressBar mProgressBar;
-    private ImageButton mPhotoPickerButton;
-    private EditText mMessageEditText;
-    private Button mSendButton;
+  private ListView mMessageListView;
+  private MessageAdapter mMessageAdapter;
+  private ProgressBar mProgressBar;
+  private ImageButton mPhotoPickerButton;
+  private EditText mMessageEditText;
+  private Button mSendButton;
 
-    private String mUsername;
-    private FirebaseDatabase mFriFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
+  private String mUsername;
+  private FirebaseDatabase mFriFirebaseDatabase;
+  private DatabaseReference mDatabaseReference;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        mUsername = ANONYMOUS;
+    mUsername = ANONYMOUS;
 
-        mFriFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFriFirebaseDatabase.getReference("messages");
+    mFriFirebaseDatabase = FirebaseDatabase.getInstance();
+    mDatabaseReference = mFriFirebaseDatabase.getReference("messages");
 
-        // Initialize references to views
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mMessageListView = (ListView) findViewById(R.id.messageListView);
-        mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
-        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
-        mSendButton = (Button) findViewById(R.id.sendButton);
+    // Initialize references to views
+    mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+    mMessageListView = (ListView) findViewById(R.id.messageListView);
+    mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
+    mMessageEditText = (EditText) findViewById(R.id.messageEditText);
+    mSendButton = (Button) findViewById(R.id.sendButton);
 
-        // Initialize message ListView and its adapter
-        List<FriendlyMessage> friendlyMessages = new ArrayList<>();
-        mMessageAdapter = new MessageAdapter(this, R.layout.item_message, friendlyMessages);
-        mMessageListView.setAdapter(mMessageAdapter);
+    // Initialize message ListView and its adapter
+    List<FriendlyMessage> friendlyMessages = new ArrayList<>();
+    mMessageAdapter = new MessageAdapter(this, R.layout.item_message, friendlyMessages);
+    mMessageListView.setAdapter(mMessageAdapter);
 
-        // Initialize progress bar
-        mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+    // Initialize progress bar
+    mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
-        // ImagePickerButton shows an image picker to upload a image for a message
-        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
-            }
+    // ImagePickerButton shows an image picker to upload a image for a message
+    mPhotoPickerButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            // TODO: Fire an intent to show an image picker
+          }
         });
 
-        // Enable Send button when there's text to send
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
+    // Enable Send button when there's text to send
+    mMessageEditText.addTextChangedListener(
+        new TextWatcher() {
+          @Override
+          public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
-                } else {
-                    mSendButton.setEnabled(false);
-                }
+          @Override
+          public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if (charSequence.toString().trim().length() > 0) {
+              mSendButton.setEnabled(true);
+            } else {
+              mSendButton.setEnabled(false);
             }
+          }
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
+          @Override
+          public void afterTextChanged(Editable editable) {}
         });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+    mMessageEditText.setFilters(
+        new InputFilter[] {new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
     // Send button sends a message and clears the EditText
     mSendButton.setOnClickListener(
@@ -122,17 +121,17 @@ public class MainActivity extends AppCompatActivity {
             mMessageEditText.setText("");
           }
         });
-    }
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    return super.onOptionsItemSelected(item);
+  }
 }
