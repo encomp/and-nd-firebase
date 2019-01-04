adcd7bc7adf407f34a5d39159010a88ada1c58d5
encomp <edgar.martinez.rico@gmail.com>
2019-01-04 08:46:54 -0800

    Add cancel sign in functionality.

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -13,6 +13,7 @@
  */
 package com.google.firebase.udacity.friendlychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -29,6 +30,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -161,6 +163,19 @@ public class MainActivity extends AppCompatActivity {
             }
           }
         };
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == RC_SIGN_IN) {
+      if (resultCode == RESULT_OK) {
+        Toast.makeText(this, "Sing in!!!", Toast.LENGTH_LONG).show();
+      } else if (resultCode == RESULT_CANCELED) {
+        Toast.makeText(this, "Sing in cancelled.", Toast.LENGTH_LONG).show();
+        finish();
+      }
+    }
   }
 
   @Override
